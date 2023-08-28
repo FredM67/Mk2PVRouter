e07f6352090f8b0f3c63c0094fb7a346c065c000
Frédéric Metrich <45318189+FredM67@users.noreply.github.com>
2023-08-28 16:10:10 +0200

    Fix

--- content/conf.py
+++ content/conf.py
@@ -11,6 +11,8 @@ import sphinx.ext.mathjax
 project = 'Mk2PVRouter'
 copyright = '2023, ENERGIE ROUTER & F. Metrich (aka FredM67)'
 author = 'ENERGIE ROUTER & F. Metrich (aka FredM67)'
+github_version = "main"
+conf_py_path = "/content/" # with leading and trailing slash
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -25,8 +27,20 @@ extensions = [
 
 source_suffix = ['.rst', '.md']
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = ['README*', '_build', 'Thumbs.db', '.DS_Store']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+	"README*",
+	"_build",
+	"Thumbs.db",
+	".DS_Store",
+    "jupyter_execute",
+    "*venv*",
+]
 
 language = 'fr'
 
@@ -34,4 +48,24 @@ language = 'fr'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+# HTML context:
+from os.path import basename, dirname, realpath
+html_context = {
+    "display_github": True,
+    "github_user": github_user,
+    # Auto-detect directory name.  This can break, but
+    # useful as a default.
+    "github_repo": github_repo_name or basename(dirname(realpath(__file__))),
+    "github_version": github_version,
+    "conf_py_path": conf_py_path,
+}
+
+import os
+if os.environ.get('GITHUB_REF', '') == 'refs/heads/'+github_version:
+    html_js_files = [
+        ('https://plausible.cs.aalto.fi/js/script.js', {"data-domain": "fredm67.github.io", "defer": "defer"}),
+    ]
