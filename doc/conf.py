e026a257d64b687342b04b6dfbfb68fc1ccf712c
Frédéric Metrich <45318189+FredM67@users.noreply.github.com>
2023-08-28 14:42:58 +0200

    Try

--- doc/conf.py
+++ doc/conf.py
@@ -3,7 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sphinx_rtd_theme
 import sphinx.ext.mathjax
 
 # -- Project information -----------------------------------------------------
