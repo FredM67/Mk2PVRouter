e8387a0c5be63b35c2662c90e54c62d5cc07ccf4
Frédéric Metrich <45318189+FredM67@users.noreply.github.com>
2023-10-04 10:29:06 +0200

    Fix tz

--- content/conf.py
+++ content/conf.py
@@ -4,6 +4,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import sphinx.ext.mathjax
+import zoneinfo
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
