37b71b7d0a9230a1157a6839897584f9593c79a9
Frédéric Metrich <frederic.metrich@live.fr>
2024-06-30 21:39:11 +0200

    Another try

--- content/conf.py
+++ content/conf.py
@@ -146,3 +146,23 @@ pyppeteer_pdf_options = {
         'right': '10mm'
     }
 }
+
+latex_engine = "xelatex"
+latex_elements = {
+    'papersize': 'a4paper',
+    'fontpkg': r'''
+\setmainfont{DejaVu Serif}
+\setsansfont{DejaVu Sans}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
+}
+latex_show_urls = 'footnote'
